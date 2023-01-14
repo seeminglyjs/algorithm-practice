dde68f89b00de7f5aa67d34d0e47ddc8ac05ac45
Jang Sung-woo <sexyhogoo@gmail.com>
2023-01-14 13:17:35 +0900

    20230114깃허브 연동 커밋

---
+++ src/test/java/com/algorithm/practice/algorithmpractice/programmers/Test_01.java
@@ -0,0 +1,14 @@
+package com.algorithm.practice.algorithmpractice.programmers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test_01 {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello test");
+	}
+
+}
